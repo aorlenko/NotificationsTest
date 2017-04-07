936f2ac29193ac9ef4a67132fc6b6a2063dfda6d
Andrey Orlenko <andrey.orlenko@evolvice.de>
2017-04-07 13:22:36 +0300

    Add MessageRecipient class

---
+++ src/NotificationsTest.DataAccess/Models/MessageRecipient.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationsTest.DataAccess.Models
+{
+    public class MessageRecipient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MessageId { get; set; }
+        public Message Message { get; set; }
+    }
+}
